0c59ba03ed70b29417b93e0ef78755078b1805e0
Luke Bui <lukenhatbui@gmail.com>
2022-10-11 21:18:31 +0700

    Create client and server modules

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onBeforeMount, ref } from "vue";
+
+const textFromServer = ref("");
+
+onBeforeMount(async () => {
+  textFromServer.value = await (await fetch("http://localhost:3000")).text();
+});
+</script>
+
+<template>
+  <main>
+    {{ textFromServer }}
+  </main>
+</template>
