340ad4b859b7c1f95caf5baa4f74df944528d17c
Luke Bui <lukenhatbui@gmail.com>
2022-10-30 20:46:20 +0700

    Minor UI changes

--- client/src/views/ReferencesView.vue
+++ client/src/views/ReferencesView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import AppCard from "@/components/AppCard.vue";
 import AppDefaultLayout from "@/components/AppDefaultLayout.vue";
 import AppLink from "@/components/AppLink.vue";
 import { RouteNames } from "@/enums";
@@ -14,22 +15,28 @@ const items: NavigationBarLinkGroup[] = [
 
 <template>
   <AppDefaultLayout>
-    <div class="default-spacing flex flex-wrap gap-12 sm:gap-24">
-      <div v-for="(item, itemIndex) in items" :key="itemIndex">
-        <div>
-          <h3 class="text-base font-medium text-white">{{ item.name }}</h3>
-          <ul role="list" class="mt-4 space-y-2">
-            <li v-for="(link, linkIndex) in item.children" :key="linkIndex">
-              <AppLink
-                class="text-base text-gray-300 hover:text-white"
-                :to="link.to"
-              >
-                {{ link.name }}
-              </AppLink>
-            </li>
-          </ul>
+    <div class="default-spacing">
+      <AppCard>
+        <div class="flex flex-wrap gap-12 sm:gap-24">
+          <div v-for="(item, itemIndex) in items" :key="itemIndex">
+            <div>
+              <h3 class="text-base font-medium dark:text-white">
+                {{ item.name }}
+              </h3>
+              <ul role="list" class="mt-4 space-y-2">
+                <li v-for="(link, linkIndex) in item.children" :key="linkIndex">
+                  <AppLink
+                    class="rounded text-base text-gray-600 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-gray-300 dark:hover:text-white"
+                    :to="link.to"
+                  >
+                    {{ link.name }}
+                  </AppLink>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
+      </AppCard>
     </div>
   </AppDefaultLayout>
 </template>
