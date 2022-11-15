1fa100f77d3477d10e5300dfcbed748d16997546
Luke Bui <lukenhatbui@gmail.com>
2022-11-15 10:02:13 +0700

    Auto-position MenuItems

--- client/src/components/Base/Elements/AppMenu.vue
+++ client/src/components/Base/Elements/AppMenu.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import type { MenuAction } from "@/types";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
-import type { PropType } from "vue";
+import { useElementBounding, useWindowSize } from "@vueuse/core";
+import { computed, ref, type PropType } from "vue";
 import AppButton from "./AppButton.vue";
 import AppLink from "./AppLink.vue";
 
@@ -13,6 +14,35 @@ defineProps({
 const isFunction = (action: MenuAction) => {
   return typeof action.action === "function";
 };
+
+const menuItems = ref(null);
+
+const windowSize = useWindowSize();
+
+const menuItemsBounding = useElementBounding(menuItems);
+
+const menuItemsPosition = computed(() => {
+  let top = false;
+  let right = false;
+
+  if (menuItemsBounding.left.value < 0) {
+    right = true;
+  }
+
+  if (menuItemsBounding.bottom.value > windowSize.height.value) {
+    top = true;
+  }
+
+  if (top && right) {
+    return "top-right";
+  } else if (top && !right) {
+    return "top-left";
+  } else if (!top && right) {
+    return "bottom-right";
+  } else {
+    return "bottom-left";
+  }
+});
 </script>
 
 <template>
@@ -33,9 +63,17 @@ const isFunction = (action: MenuAction) => {
       leave-to-class="transform opacity-0 scale-95"
     >
       <MenuItems
-        class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800 dark:ring-gray-700"
+        class="absolute z-10 w-56 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800 dark:ring-gray-700"
+        :class="{
+          'right-0 mt-2 origin-top-right': menuItemsPosition === 'bottom-left',
+          'left-0 mt-2 origin-top-left': menuItemsPosition === 'bottom-right',
+          'right-0 bottom-full mb-2 origin-bottom-right':
+            menuItemsPosition === 'top-left',
+          'left-0 bottom-full mb-2 origin-bottom-left':
+            menuItemsPosition === 'top-right',
+        }"
       >
-        <div class="py-1">
+        <div ref="menuItems" class="py-1">
           <MenuItem
             v-for="(action, actionIndex) in actions"
             :key="actionIndex"
